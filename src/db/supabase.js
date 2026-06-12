537b0e2013423f093b225c8f6f1d2fd436944c95
Melkysedeth <shuek_1987@hotmail.com>
2026-06-12 15:20:49 -0500

    chore: env vars y redirects para Netlify

--- src/db/supabase.js
+++ src/db/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://vtozkuoyptmjbacjlbxt.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ0b3prdW95cHRtamJhY2psYnh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODExMTc4NDIsImV4cCI6MjA5NjY5Mzg0Mn0.9QkYul2OwXjNa8q20t4G_ccffBoL1_nhffh9AMdTrhI'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+export const supabase = createClient(
+    import.meta.env.VITE_SUPABASE_URL,
+    import.meta.env.VITE_SUPABASE_ANON_KEY
+)
